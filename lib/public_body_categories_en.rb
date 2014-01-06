dddf32b0b1fa524dd78fb6f655ff815c7693326e
utvk <utvk@users.noreply.github.com>
2014-01-06 17:06:47 +0100

    Added some norwegian categories

--- lib/public_body_categories_en.rb
+++ lib/public_body_categories_en.rb
@@ -14,3 +14,22 @@ PublicBodyCategories.add(:en, [
     "Popular agencies",
         [ "popular_agency", "Popular agencies", "a lonely agency"]
 ])
+
+PublicBodyCategories.add(:nb_NO, [
+    "Populare myndigheter",
+        [ "popular_agency", "Populære instanser", "a lonely agency"],
+    "Utøvende makt",
+        [ "27", "Departementene", "et departement" ],
+        [ "33", "Direktorat og tilsyn", "et direktorat eller tilsyn"],
+        [ "38", "Andre sentrale enheter direkte underlagt dep.", "en sentral enhet underlagt departement"],
+        [ "39", "Andre regionale og lokale enheter underlagt dep.", "en regional eller lokal enhet underlagt departement"],
+        [ "37", "Lokale direktoratsenheter", "en lokal direktoratsenhet"],
+        [ "36", "Regionale direktoratsenheter", "en regional direktoratsenhet"],
+        [ "76", "Ambassader og utenriksstasjoner", "en ambassade eller utenriksstasjon"],
+        [ "68", "Andre statlige instanser", "en statlig instans"],
+    "Kommuner",
+        [ "66", "Kommunene", "en kommune"],
+        [ "73", "Kommunale driftsenheter", "en kommunal driftsenhet"],
+    "Dømmende makt",
+        [ "19", "Tingrettene og byfogdembetene", "en tingrett eller byfodgembetene"],
+])
